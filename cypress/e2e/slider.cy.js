ac33a3fabed0d32ad1b60b298a9456f554047e80
Piotr Dajewski <piterd1998@o2.pl>
2024-04-17 11:25:52 +0200

    tests and hotjar

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,20 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Navigation Test', function () {
+  it('Allows user to navigate through the gallery using navigation buttons', function () {
+    cy.visit('http://localhost:3000');
+
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+
+    cy.get('.swiper-button-prev').click({ force: true });
+    
+    cy.wait(2000); 
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  });
+});
+
+
